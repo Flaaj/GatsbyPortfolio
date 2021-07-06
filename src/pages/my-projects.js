37c9be09a064f5af8a9a85224ee9fff62a09bb9e
Michał Wołek <32792800+Flaaj@users.noreply.github.com>
2021-07-06 07:47:35 +0200

    updated project links

--- src/pages/my-projects.js
+++ src/pages/my-projects.js
@@ -28,23 +28,29 @@ const AboutPage = () => (
                     corner) is found. The elements are displayed using CSS Grid"
                         technologies="HTML, CSS, CSS Grid, JavaScript (ES6), Depth first search algorithm"
                     />
+                </li>
+                <li className="project__single">
                     <Project
                         name="Best Shop"
                         thumbnail="bestshop.png"
-                        link="/bestshop/index.html"
+                        link="https://flaaj-bestshop.netlify.app/"
                         githubLink="https://github.com/flaaj/bestshop"
                         description="
             A responsive web page written using HTML, Sass for styling and a little JavaScript. The page was recreated based on AdobeXD project with almost pixel-perfect precision"
                         technologies="HTML, Sass, Responsive Web Design, JavaScript"
                     />
+                </li>
+                <li className="project__single">
                     <Project
                         name="Tetris"
                         thumbnail="tetris.png"
-                        link="/tetris/index.html"
+                        link="https://flaaj-tetris.netlify.app/"
                         githubLink="https://github.com/flaaj/tetris"
                         description="A simple tetris game written in ES6, using canvas to display the blocks. The game's little 'engine' was written by myself"
                         technologies="HTML Canvas, Javascript"
                     />
+                </li>
+                <li className="project__single">
                     <Project
                         name="Watch Tuna"
                         thumbnail="watchtuna.png"
@@ -53,6 +59,8 @@ const AboutPage = () => (
                         description="This is Watch Tuna, a web app that helps watch owners with no professional tools to tune their watches or at least determine how much their watches miss the actual time. It uses microphone to record the soundwave of balance wheel, then seeks for peaks in the soundwave, calculates frequency of ticking as accurately as possible, then compares it to actual required frequency of a watch and calculates how many seconds per day will the watch lose or gain."
                         technologies="JavaScript, React, Sass, sound wave filtering, peak finding algorithm"
                     />
+                </li>
+                <li className="project__single">
                     <Project
                         name="Image search app"
                         thumbnail="imagesearch.png"
