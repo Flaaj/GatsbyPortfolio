c4bb35959890424ca4c7728d42a10f03cdac0480
unknown <michal.wolek@gogomedia.pl>
2021-07-21 13:33:28 +0200

    adding navigation cms fields

--- src/cms/components/navigation.js
+++ src/cms/components/navigation.js
@@ -2,8 +2,33 @@ const navigation = {
     label: "Menu",
     name: "navigation",
     fields: [
-        
-    ]
-}
+        {
+            label: "Menu Items",
+            name: "menu_items",
+            widget: "list",
+            fields: [
+                {
+                    label: "Menu Item Name",
+                    name: "menu_item_name",
+                    widget: "string",
+                    required: true,
+                },
+                {
+                    label: "Menu Item Link",
+                    name: "menu_item_link",
+                    widget: "string",
+                    required: true,
+                },
+                {
+                    label: "Internal",
+                    name: "menu_item_link",
+                    hint: "Does this link lead to this domain?",
+                    widget: "boolean",
+                    required: true,
+                },
+            ],
+        },
+    ],
+};
 
 export default navigation;
