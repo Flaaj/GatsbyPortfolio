25b66da120e8e84696908598c291b02f03be6754
unknown <michal.wolek@gogomedia.pl>
2021-07-21 15:14:02 +0200

    trying richtext wysiwyg editor

--- src/cms/index.js
+++ src/cms/index.js
@@ -2,12 +2,12 @@ import CMS from "netlify-cms-app";
 import pages from "./pages";
 import components from "./components";
 
-import richText from "netlify-cms-widget-richtext";
+import * as richText from "netlify-cms-widget-richtext";
 const {RichTextControl, RichTextPreview} = richText;
 
 window.CMS_MANUAL_INIT = true;
 
-CMS.registerWidget("richtext", RichTextControl);
+CMS.registerWidget("richtext", RichTextControl, RichTextPreview);
 CMS.init({
     config: {
         load_config_file: false,
