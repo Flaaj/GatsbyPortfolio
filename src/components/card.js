3792b718e0150b3a51d4183832519b051571c796
unknown <michal.wolek@gogomedia.pl>
2021-07-21 11:44:23 +0200

    added contact page fields

--- src/components/card.js
+++ src/components/card.js
@@ -4,7 +4,7 @@ import CardItem from "./card-item";
 const Card = ({ items }) => (
     <div className="card">
         {items.map(({ label, link, text }) => (
-            <CardItem label={label} link={link} text={text} />
+            <CardItem key={link} label={label} link={link} text={text} />
         ))}
     </div>
 );
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,28 @@
 import * as React from "react";
 import Layout from "../layouts/layout";
 import Card from "../components/card";
+import { graphql } from "gatsby";
 
-const Contact = () => {
+export const query = graphql`
+    query {
+        markdownRemark {
+            frontmatter {
+                contact_page_title
+                contact_fields {
+                    contact_link_text
+                    label
+                    contact_link_url
+                }
+            }
+        }
+    }
+`;
+
+const Contact = ({data}) => {
+    console.log(data)
     return (
         <Layout>
-            <h1 class="title">Here are some ways to contact me:</h1>
+            <h1 className="title">Here are some ways to contact me:</h1>
             <Card items={items} />
         </Layout>
     );
@@ -17,7 +34,7 @@ const items = [
     {
         label: "Phone",
         link: "tel: +48604606621",
-        text: "604 606 621"
+        text: "604 606 621",
     },
     {
         label: "Email",
@@ -36,5 +53,4 @@ const items = [
         link: "/CV_Michal_Wolek.pdf",
         text: "Get my CV",
     },
-
 ];
